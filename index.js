57b99cadec0ae2900878d7cd636f27f9564546d1
Karthik Venkatachalam <karthik.venkatachalam.ext@nokia.com>
2025-03-06 09:25:54 +0530

    ENV url whitespace issue

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 const server = http.createServer(app); // Create HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "https://kartwise-app-api-integration.onrender.com", // React app URL
+    origin: process.env.URL.trim(), // React app URL
     credentials: true, // Allow cookies to be sent
   },
 });
@@ -60,7 +60,7 @@ server.listen(8800, () => {
   export { io };
 
 app.use(cors({
-    origin: "https://kartwise-app-api-integration.onrender.com", // Your React app URL
+    origin: process.env.URL.trim(), // Your React app URL
     credentials: true, // Allow cookies to be sent
   }));
   
